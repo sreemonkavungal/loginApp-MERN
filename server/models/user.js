d2e28cd6383cc172c4ec5c77f75732587d4735fc
SREEMON KS <90373393+sreemonkavungal@users.noreply.github.com>
2023-06-08 12:17:49 +0530

    Add files via upload

---
+++ server/models/user.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const Joi = require("joi");
+const passwordComplexity = require("joi-password-complexity");
+const secretOrPrivateKey = process.env.JWT_SECRET; 
+// const secretOrPrivateKey = "your-secret-or-private-key";
+
+
+const userSchema = new mongoose.Schema({
+	firstName: { type: String, required: true },
+	lastName: { type: String, required: true },
+	email: { type: String, required: true },
+	password: { type: String, required: true },
+});
+
+userSchema.methods.generateAuthToken = function () {
+	const token = jwt.sign({ _id: this._id }, secretOrPrivateKey, {
+		expiresIn: "50d",
+	});
+	return token;
+};
+
+const User = mongoose.model("user", userSchema);
+
+const validate = (data) => {
+	const schema = Joi.object({
+		firstName: Joi.string().required().label("First Name"),
+		lastName: Joi.string().required().label("Last Name"),
+		email: Joi.string().email().required().label("Email"),
+		password: passwordComplexity().required().label("Password"),
+	});
+	return schema.validate(data);
+};
+
+module.exports = { User, validate };
